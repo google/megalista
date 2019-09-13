fa1712928c4a4c28c6a3ba2aa2f400e4430facec
Álvaro Stivi <astivi@google.com>
2019-09-13 12:26:07 -0300

    Updates requirements file to contain all requirements

--- generate_megalist_token.sh
+++ generate_megalist_token.sh
@@ -13,16 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cd megalist_dataflow
-pip3 install --user -r requirements.txt
-python3 -m main --runner DataflowRunner --project vuru-beacons-test --gcp_project_id vuru-beacons-test --temp_location gs://megalist-data/tmp/  --setup_file ./setup.py --template_location gs://megalist-data/templates/megalist
-gsutil cp megalist_metadata gs://megalist-data/templates/megalist_metadata
-cd ..
-cd cloud_functions
-cd is_new_buyer
-gcloud functions deploy is_new_buyer --runtime python37 --trigger-http
-cd ..
-cd ..
-cd examples
-gsutil cp generate_megalist_token.py gs://megalist-data/generate_megalist_token.py
-cd ..
+python3 cloud_config/generate_megalist_token.py --client_id $1 --client_secret $2
