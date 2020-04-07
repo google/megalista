2a9229edbc852c29bfc6088b75a1a5e44902935f
Antônio Moreira <antoniomoreira@google.com>
2020-04-07 15:26:53 -0300

    Update GoogleAdsUserListUploaderDoFn to receive an Execution on the process method, no on the create_bundle method

--- megalist_dataflow/uploaders/google_ads_user_list_uploader.py
+++ megalist_dataflow/uploaders/google_ads_user_list_uploader.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Google LLC
+# Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,127 +17,187 @@ import logging
 
 
 class GoogleAdsUserListUploaderDoFn(beam.DoFn):
-    crm_list_name = 'Megalist - CRM - Buyers'
-    mobile_list_name = 'Megalist - Mobile - Buyers'
-    rev_list_name = 'Megalist - Potential New Buyers'
 
-    def __init__(self, oauth_credentials, developer_token, customer_id, app_id):
-        self.oauth_credentials = oauth_credentials
-        self.developer_token = developer_token
-        self.customer_id = customer_id
-        self.app_id = app_id
-        self.active = True
-        if self.developer_token is None or self.customer_id is None:
-            self.active = False
+  def __init__(self, oauth_credentials, developer_token, customer_id, app_id):
+    super().__init__()
+    self.oauth_credentials = oauth_credentials
+    self.developer_token = developer_token
+    self.customer_id = customer_id
+    self.app_id = app_id
+    self.active = True
+    if self.developer_token is None or self.customer_id is None:
+      self.active = False
 
-    def _get_user_list_service(self):
-        from googleads import adwords
-        from googleads import oauth2
-        oauth2_client = oauth2.GoogleRefreshTokenClient(
-            self.oauth_credentials.get_client_id(), self.oauth_credentials.get_client_secret(), self.oauth_credentials.get_refresh_token())
-        client = adwords.AdWordsClient(
-            self.developer_token.get(), oauth2_client, 'MegaList Dataflow', client_customer_id=self.customer_id.get())
-        return client.GetService(
-            'AdwordsUserListService', 'v201809')
+  def start_bundle(self):
+    pass
 
-    def _create_list_if_it_does_not_exist(self, user_list_service, list_name, list_definition):
-        response = user_list_service.get([{
-            'fields': ['Id', 'Name'],
-            'predicates': [{
-                'field': 'Name',
-                'operator': 'EQUALS',
-                'values': [list_name]
-            }]
-        }])
-        if (len(response.entries) == 0):
-            logging.getLogger().info('%s list does not exist, creating...' % list_name)
-            result = user_list_service.mutate([{
-                'operator': 'ADD',
-                **list_definition
-            }])
-            id = result['value'][0]['id']
-            logging.getLogger().info('%s created with id: %d' % (list_name, id))
-        else:
-            id = response.entries[0]['id']
-            logging.getLogger().info('Found %s with id: %d' % (list_name, id))
-        return id
+  def _get_user_list_service(self):
+    from googleads import adwords
+    from googleads import oauth2
+    oauth2_client = oauth2.GoogleRefreshTokenClient(
+      self.oauth_credentials.get_client_id(), self.oauth_credentials.get_client_secret(),
+      self.oauth_credentials.get_refresh_token())
+    client = adwords.AdWordsClient(self.developer_token.get(), oauth2_client, 'MegaList Dataflow',
+                                   client_customer_id=self.customer_id.get())
+    return client.GetService('AdwordsUserListService', 'v201809')
 
-    def start_bundle(self):
-        if self.active == False:
-            logging.getLogger().warning("Skipping upload to ads, parameters not configured.")
-            return
-        user_list_service = self._get_user_list_service()
-        self.user_list_id = self._create_list_if_it_does_not_exist(user_list_service, self.crm_list_name, {
-            'operand': {
-                'xsi_type': 'CrmBasedUserList',
-                'name': self.crm_list_name,
-                'description': self.crm_list_name,
-                # CRM-based user lists can use a membershipLifeSpan of 10000 to indicate
-                # unlimited; otherwise normal values apply.
-                'membershipLifeSpan': 10000,
-                'uploadKeyType': 'CONTACT_INFO'
+  @staticmethod
+  def _create_list_if_it_does_not_exist(user_list_service, list_name, list_definition):
+    response = user_list_service.get([{
+      'fields': ['Id', 'Name'],
+      'predicates': [{
+        'field': 'Name',
+        'operator': 'EQUALS',
+        'values': [list_name]
+      }]
+    }])
+    if (len(response.entries) == 0):
+      logging.getLogger().info('%s list does not exist, creating...' % list_name)
+      result = user_list_service.mutate([{
+        'operator': 'ADD',
+        **list_definition
+      }])
+      id = result['value'][0]['id']
+      logging.getLogger().info('List %s created with id: %d' % (list_name, id))
+    else:
+      id = response.entries[0]['id']
+      logging.getLogger().info('List found %s with id: %d' % (list_name, id))
+    return id
+
+  def _create_lists(self, crm_list_name, mobile_list_name, rev_list_name):
+    return self._do_create_lists(
+      self._get_user_list_service(),
+      self.app_id.get(),
+      crm_list_name,
+      mobile_list_name,
+      rev_list_name)
+
+  @staticmethod
+  def _do_create_lists(user_list_service, app_id, crm_list_name, mobile_list_name, rev_list_name):
+    _create_list_if_it_does_not_exist = GoogleAdsUserListUploaderDoFn._create_list_if_it_does_not_exist
+
+    user_list_id = _create_list_if_it_does_not_exist(user_list_service, crm_list_name, {
+      'operand': {
+        'xsi_type': 'CrmBasedUserList',
+        'name': crm_list_name,
+        'description': crm_list_name,
+        # CRM-based user lists can use a membershipLifeSpan of 10000 to indicate
+        # unlimited; otherwise normal values apply.
+        'membershipLifeSpan': 10000,
+        'uploadKeyType': 'CONTACT_INFO'
+      }
+    })
+
+    mobile_user_list_id = _create_list_if_it_does_not_exist(user_list_service, mobile_list_name, {
+      'operand': {
+        'xsi_type': 'CrmBasedUserList',
+        'name': mobile_list_name,
+        'description': mobile_list_name,
+        # CRM-based user lists can use a membershipLifeSpan of 10000 to indicate
+        # unlimited; otherwise normal values apply.
+        'membershipLifeSpan': 10000,
+        'appId': app_id,
+        'uploadKeyType': 'MOBILE_ADVERTISING_ID'
+      }
+    })
+
+    _create_list_if_it_does_not_exist(user_list_service, rev_list_name, {
+      'operand': {
+        'xsi_type': 'LogicalUserList',
+        'name': rev_list_name,
+        'description': rev_list_name,
+        'status': 'OPEN',
+        'rules': [{
+          'operator': 'NONE',
+          'ruleOperands': [{
+            'UserList': {
+              'id': user_list_id,
+              'xsi_type': 'CrmBasedUserList'
             }
-        })
-        self.mobile_user_list_id = self._create_list_if_it_does_not_exist(user_list_service, self.mobile_list_name, {
-            'operand': {
-                'xsi_type': 'CrmBasedUserList',
-                'name': self.mobile_list_name,
-                'description': self.mobile_list_name,
-                # CRM-based user lists can use a membershipLifeSpan of 10000 to indicate
-                # unlimited; otherwise normal values apply.
-                'membershipLifeSpan': 10000,
-                'appId': self.app_id.get(),
-                'uploadKeyType': 'MOBILE_ADVERTISING_ID'
-            }
-        })
-        self._create_list_if_it_does_not_exist(user_list_service, self.rev_list_name, {
-            'operand': {
-                'xsi_type': 'LogicalUserList',
-                'name': self.rev_list_name,
-                'description': self.rev_list_name,
-                'status': 'OPEN',
-                'rules': [{
-                    'operator': 'NONE',
-                    'ruleOperands': [{
-                        'UserList': {
-                            'id': self.user_list_id,
-                            'xsi_type': 'CrmBasedUserList'
-                        }
-                    }, {
-                        'UserList': {
-                            'id': self.mobile_user_list_id,
-                            'xsi_type': 'CrmBasedUserList'
-                        }}]
-                }]
-            }
-        })
+          }, {
+            'UserList': {
+              'id': mobile_user_list_id,
+              'xsi_type': 'CrmBasedUserList'
+            }}]
+        }]
+      }
+    })
 
-    def process(self, element):
-        if self.active == False:
-            return
-        user_list_service = self._get_user_list_service()
+    return user_list_id, mobile_user_list_id
 
-        mobile_ids = [{'mobileId': row['mobileId']} for row in element]
+  @staticmethod
+  def _assert_elements_have_same_execution(elements):
+    last_execution = elements[0]['execution']
+    for element in elements:
+      current_execution = element['execution']
+      if current_execution != last_execution:
+        raise ValueError(
+          'At least two Execution in a single call ({}) and ({})'.format(str(current_execution), str(last_execution)))
+      last_execution = current_execution
 
-        mutate_mobile_members_operation = {
-            'operand': {
-                'userListId': self.mobile_user_list_id,
-                'membersList': mobile_ids
-            },
-            'operator': 'ADD'
-        }
-        user_list_service.mutateMembers([mutate_mobile_members_operation])
+  @staticmethod
+  def _assert_all_list_names_are_present(any_execution):
+    destination = any_execution.destination_metadata
+    if len(destination) is not 3:
+      raise ValueError('Missing destination information. Found {}'.format(len(destination)))
 
-        for row in element:
-            row.pop('mobileId', None)
+    if not destination[0] \
+        or not destination[1] \
+        or not destination[2]:
+      raise ValueError('Missing destination information. Received {}'.format(str(destination)))
 
-        mutate_members_operation = {
-            'operand': {
-                'userListId': self.user_list_id,
-                'membersList': element
-            },
-            'operator': 'ADD'
-        }
-        user_list_service.mutateMembers([mutate_members_operation])
+  def process(self, elements, **kwargs):
+    """
+    Args:
+       elements: List of dict with two elements: 'execution' and 'row'. All executions must be equal.
+    """
+    if not self.active:
+      logging.getLogger().warning('Skipping upload to ads, parameters not configured.')
+      return
 
-        return element
+    if len(elements) == 0:
+      logging.getLogger().warning('Skipping upload to ads, received no elements.')
+      return
+
+    self._assert_elements_have_same_execution(elements)
+    any_execution = elements[0]['execution']
+    self._assert_all_list_names_are_present(any_execution)
+
+    user_list_id, mobile_user_list_id = self._create_lists(any_execution.destination_metadata[0],
+                                                           any_execution.destination_metadata[1],
+                                                           any_execution.destination_metadata[2])
+
+    user_list_service = self._get_user_list_service()
+
+    self._do_upload(user_list_service, self._extract_rows(elements), mobile_user_list_id, user_list_id)
+
+  @staticmethod
+  def _extract_rows(elements):
+    return [dict['row'] for dict in elements]
+
+  @staticmethod
+  def _do_upload(user_list_service, rows, mobile_user_list_id, user_list_id):
+    mobile_ids = [{'mobileId': row['mobileId']} for row in rows]
+
+    mutate_mobile_members_operation = {
+      'operand': {
+        'userListId': mobile_user_list_id,
+        'membersList': mobile_ids
+      },
+      'operator': 'ADD'
+    }
+
+    user_list_service.mutateMembers([mutate_mobile_members_operation])
+
+    for row in rows:
+      row.pop('mobileId', None)
+
+    mutate_members_operation = {
+      'operand': {
+        'userListId': user_list_id,
+        'membersList': rows
+      },
+      'operator': 'ADD'
+    }
+
+    user_list_service.mutateMembers([mutate_members_operation])
--- test/uploaders/google_ads_user_list_uploader_test.py
+++ test/uploaders/google_ads_user_list_uploader_test.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Google LLC
+# Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,65 +15,182 @@
 from megalist_dataflow.uploaders.google_ads_user_list_uploader import GoogleAdsUserListUploaderDoFn
 from megalist_dataflow.utils.oauth_credentials import OAuthCredentials
 from apache_beam.options.value_provider import StaticValueProvider
-from googleads import adwords
-from googleads import oauth2
 import pytest
+
+from utils.execution import Execution, OriginType, Action
 
 
 @pytest.fixture
 def uploader(mocker):
-    mocker.patch('googleads.oauth2.GoogleRefreshTokenClient')
-    mocker.patch('googleads.adwords.AdWordsClient')
-    id = StaticValueProvider(str, "id")
-    secret = StaticValueProvider(str, "secret")
-    access = StaticValueProvider(str, "access")
-    refresh = StaticValueProvider(str, "refresh")
-    credentials = OAuthCredentials(id, secret, access, refresh)
-    return GoogleAdsUserListUploaderDoFn(credentials, StaticValueProvider(str, "devtoken"), StaticValueProvider(str, "123-456-7890"), StaticValueProvider(str, "com.app.id"))
+  mocker.patch('googleads.oauth2.GoogleRefreshTokenClient')
+  mocker.patch('googleads.adwords.AdWordsClient')
+  id = StaticValueProvider(str, "id")
+  secret = StaticValueProvider(str, "secret")
+  access = StaticValueProvider(str, "access")
+  refresh = StaticValueProvider(str, "refresh")
+  credentials = OAuthCredentials(id, secret, access, refresh)
+  return GoogleAdsUserListUploaderDoFn(credentials, StaticValueProvider(str, "devtoken"),
+                                       StaticValueProvider(str, "123-456-7890"), StaticValueProvider(str, "com.app.id"))
 
 
 def test_get_service(mocker, uploader):
-    assert uploader._get_user_list_service() != None
+  assert uploader._get_user_list_service() is not None
 
 
-def test_not_active(mocker):
-    id = StaticValueProvider(str, "id")
-    secret = StaticValueProvider(str, "secret")
-    access = StaticValueProvider(str, "access")
-    refresh = StaticValueProvider(str, "refresh")
-    credentials = OAuthCredentials(id, secret, access, refresh)
-    uploader = GoogleAdsUserListUploaderDoFn(
-        credentials, None, "123-456-7890", "com.app.id")
-    mocker.patch.object(uploader, '_get_user_list_service')
-    uploader.start_bundle()
-    uploader.process([])
-    uploader._get_user_list_service.assert_not_called()
+def test_not_active(mocker, caplog):
+  id = StaticValueProvider(str, "id")
+  secret = StaticValueProvider(str, "secret")
+  access = StaticValueProvider(str, "access")
+  refresh = StaticValueProvider(str, "refresh")
+  credentials = OAuthCredentials(id, secret, access, refresh)
+  uploader = GoogleAdsUserListUploaderDoFn(credentials, None, "123-456-7890", "com.app.id")
+  mocker.patch.object(uploader, '_get_user_list_service')
+  uploader.process([], )
+  uploader._get_user_list_service.assert_not_called()
+  assert 'Skipping upload to ads, parameters not configured.' in caplog.text
 
 
-def test_list_created(mocker, uploader):
-    result = mocker.MagicMock()
-    result.entries = [mocker.MagicMock()]
-    mocker.patch.object(uploader, '_get_user_list_service')
-    uploader._get_user_list_service().get = mocker.Mock(return_value=result)
-    uploader.start_bundle()
-    uploader._get_user_list_service().mutate.assert_not_called()
+def test_work_with_empty_elements(mocker, caplog):
+  id = StaticValueProvider(str, "id")
+  secret = StaticValueProvider(str, "secret")
+  access = StaticValueProvider(str, "access")
+  refresh = StaticValueProvider(str, "refresh")
+  credentials = OAuthCredentials(id, secret, access, refresh)
+  uploader = GoogleAdsUserListUploaderDoFn(credentials, "123", "123-456-7890", "com.app.id")
+  mocker.patch.object(uploader, '_get_user_list_service')
+  uploader.process([], )
+  uploader._get_user_list_service.assert_not_called()
+  assert 'Skipping upload to ads, received no elements.' in caplog.text
 
 
-def test_list_creation(mocker, uploader):
-    mocker.patch.object(uploader, '_get_user_list_service')
-    uploader.start_bundle()
-    uploader._get_user_list_service().mutate.assert_any_call([{'operator': 'ADD', 'operand': {
-        'xsi_type': 'CrmBasedUserList', 'name': 'Megalist - CRM - Buyers', 'description': 'Megalist - CRM - Buyers', 'membershipLifeSpan': 10000, 'uploadKeyType': 'CONTACT_INFO'}}])
-    uploader._get_user_list_service().mutate.assert_any_call([{'operator': 'ADD', 'operand': {'xsi_type': 'LogicalUserList', 'name': 'Megalist - Potential New Buyers',
-                                                                                              'description': 'Megalist - Potential New Buyers', 'status': 'OPEN', 'rules': [{'operator': 'NONE', 'ruleOperands': [{'UserList': {'id': mocker.ANY, 'xsi_type': 'CrmBasedUserList'}}, {'UserList': {'id': mocker.ANY, 'xsi_type': 'CrmBasedUserList'}}]}]}}])
+def test_fail_having_more_than_one_execution(mocker):
+  id = StaticValueProvider(str, "id")
+  secret = StaticValueProvider(str, "secret")
+  access = StaticValueProvider(str, "access")
+  refresh = StaticValueProvider(str, "refresh")
+  credentials = OAuthCredentials(id, secret, access, refresh)
+  uploader = GoogleAdsUserListUploaderDoFn(credentials, "123", "123-456-7890", "com.app.id")
+  mocker.patch.object(uploader, '_get_user_list_service')
+
+  exec1 = Execution('orig1', OriginType.BIG_QUERY, ('dt1', 'buyers'), Action.ADS_USER_LIST_UPLOAD, 'dest1',
+                    ('a', 'b', 'c'))
+  exec2 = Execution('origi2', OriginType.BIG_QUERY, ('dt2', 'buyers2'), Action.ADS_USER_LIST_UPLOAD, 'dest2',
+                    ('a', 'b', 'c'))
+
+  with pytest.raises(ValueError, match='At least two Execution in a single call'):
+    uploader.process([{'execution': exec1}, {'execution': exec2}])
+
+
+def test_list_already_created(mocker, uploader):
+  result = mocker.MagicMock()
+  result.entries = [mocker.MagicMock()]
+  user_list_service = mocker.MagicMock()
+  user_list_service.get = mocker.Mock(return_value=result)
+  uploader._create_list_if_it_does_not_exist(user_list_service, '', '')
+  uploader._get_user_list_service().mutate.assert_not_called()
+
+
+def test_lists_creation(mocker, uploader):
+  mocker.patch.object(uploader, '_get_user_list_service')
+
+  uploader._create_lists('crm', 'mobile', 'rev')
+  assert_crm_list_creation(uploader._get_user_list_service(), 'crm')
+  assert_mobile_list_creation(uploader._get_user_list_service(), 'mobile')
+  assert_logical_list_creation(mocker, uploader._get_user_list_service(), 'rev')
+
+
+def assert_crm_list_creation(user_list_service, list_name):
+  user_list_service.mutate.assert_any_call([{
+    'operator': 'ADD',
+    'operand': {
+      'xsi_type': 'CrmBasedUserList',
+      'name': list_name,
+      'description': list_name,
+      'membershipLifeSpan': 10000,
+      'uploadKeyType': 'CONTACT_INFO'
+    }
+  }])
+
+
+def assert_mobile_list_creation(user_list_service, list_name):
+  user_list_service.mutate.assert_any_call([{
+    'operator': 'ADD',
+    'operand': {
+      'xsi_type': 'CrmBasedUserList',
+      'name': list_name,
+      'description': list_name,
+      # CRM-based user lists can use a membershipLifeSpan of 10000 to indicate
+      # unlimited; otherwise normal values apply.
+      'membershipLifeSpan': 10000,
+      'appId': 'com.app.id',
+      'uploadKeyType': 'MOBILE_ADVERTISING_ID'
+    }
+  }])
+
+
+def assert_logical_list_creation(mocker, user_list_service, list_name):
+  user_list_service.mutate.assert_any_call([{
+    'operator': 'ADD',
+    'operand': {
+      'xsi_type': 'LogicalUserList',
+      'name': list_name,
+      'description': list_name,
+      'status': 'OPEN',
+      'rules': [{
+        'operator': 'NONE',
+        'ruleOperands': [
+          {'UserList': {'id': mocker.ANY, 'xsi_type': 'CrmBasedUserList'}},
+          {'UserList': {'id': mocker.ANY, 'xsi_type': 'CrmBasedUserList'}}]}]}}])
+
+
+def test_fail_missing_destination_metadata(uploader):
+  execution = Execution('orig1', OriginType.BIG_QUERY, ('dt1', 'buyers'), Action.ADS_USER_LIST_UPLOAD, 'dest1',
+                        ('a', 'b'))
+  with pytest.raises(ValueError, match='Missing destination information'):
+    uploader.process([{'execution': execution}])
+
+
+def test_fail_empty_first_metadata(uploader):
+  assert_empty_destination_metadata(uploader, (None, 'b', 'c'))
+  assert_empty_destination_metadata(uploader, ('', 'b', 'c'))
+
+  assert_empty_destination_metadata(uploader, ('a', None, 'c'))
+  assert_empty_destination_metadata(uploader, ('a', '', 'c'))
+
+  assert_empty_destination_metadata(uploader, ('a', 'b', None))
+  assert_empty_destination_metadata(uploader, ('a', 'b', ''))
+
+
+def assert_empty_destination_metadata(uploader, destination_metadata):
+  execution = Execution('orig1', OriginType.BIG_QUERY, ('dt1', 'buyers'), Action.ADS_USER_LIST_UPLOAD, 'dest1',
+                        destination_metadata)
+  with pytest.raises(ValueError, match='Missing destination information'):
+    uploader.process([{'execution': execution}])
 
 
 def test_element_uploading(mocker, uploader):
-    mocker.patch.object(uploader, '_get_user_list_service')
-    uploader.user_list_id = 123
-    uploader.mobile_user_list_id = 234
-    uploader.process([{'mobileId': 'a', 'email': 'x@x.com'}, {'mobileId': 'b', 'email': 'y@y.com'}])
-    uploader._get_user_list_service().mutateMembers.assert_any_call(
-        [{'operand': {'membersList': [{'mobileId': 'a'}, {'mobileId': 'b'}], 'userListId': 234}, 'operator': 'ADD'}])
-    uploader._get_user_list_service().mutateMembers.assert_any_call(
-        [{'operand': {'membersList': [{'email': 'x@x.com'}, {'email': 'y@y.com'}], 'userListId': 123}, 'operator': 'ADD'}])
+  user_list = 'user_list'
+  mobile_list = 'mobile_list'
+
+  mocker.patch.object(uploader, '_create_lists')
+  uploader._create_lists.return_value = (user_list, mobile_list)
+
+
+  execution = Execution('orig1', OriginType.BIG_QUERY, ('dt1', 'buyers'), Action.ADS_USER_LIST_UPLOAD, 'dest1',
+                        (user_list, mobile_list, 'c'))
+  uploader.process([{'execution': execution, "row": {'mobileId': 'a', 'email': 'x@x.com'}},
+                    {'execution': execution, 'row': {'mobileId': 'b', 'email': 'y@y.com'}}])
+  uploader._get_user_list_service().mutateMembers.assert_any_call(
+    [{'operand': {
+      'membersList': [
+        {'mobileId': 'a'},
+        {'mobileId': 'b'}],
+      'userListId': mobile_list},
+      'operator': 'ADD'}])
+  uploader._get_user_list_service().mutateMembers.assert_any_call(
+    [{'operand': {
+      'membersList': [
+        {'email': 'x@x.com'},
+        {'email': 'y@y.com'}],
+      'userListId': user_list},
+      'operator': 'ADD'}])
