70920d2b05718aed3a1b2a553029c81743eb357a
Álvaro Stivi <astivi@google.com>
2020-05-11 18:50:56 -0300

    Adds custom dimensions for MP
    Change-Id: I87f0ddbaab790d1febf53758671e013fb70a158e

--- megalist_dataflow/uploaders/google_analytics_measurement_protocol.py
+++ megalist_dataflow/uploaders/google_analytics_measurement_protocol.py
@@ -18,6 +18,7 @@ from urllib.parse import quote
 import requests
 import json
 import apache_beam as beam
+from typing import Dict, Any
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaInMemoryUpload
 from google.oauth2.credentials import Credentials
@@ -28,39 +29,38 @@ from utils.execution import DestinationType
 
 
 class GoogleAnalyticsMeasurementProtocolUploaderDoFn(beam.DoFn):
-  def __init__(self,
-               oauth_credentials):
-    super().__init__()
-    self.API_URL = "https://www.google-analytics.com/batch"
-    self.UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+    def __init__(self, oauth_credentials: Credentials):
+        super().__init__()
+        self.API_URL = "https://www.google-analytics.com/batch"
+        self.UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
 
-  def start_bundle(self):
-    pass
+    def start_bundle(self):
+        pass
 
-  def _format_hit(self, payload):
-    return "&".join([key + "=" + quote(str(value)) for key, value in payload.items()])
+    def _format_hit(self, payload: Dict[str, Any]) -> str:
+        return "&".join([key + "=" + quote(str(value)) for key, value in payload.items()])
 
-  @utils.safe_process(logger=logging.getLogger("megalista.GoogleAnalyticsMeasurementProtocolUploader"))
-  def process(self, elements, **kwargs):
-    any_execution = elements[0]['execution']
-    rows = utils.extract_rows(elements)
-    payloads = [
-      {
-        "v": 1,
-        "tid": any_execution.destination.destination_metadata[0],
-        "ni": any_execution.destination.destination_metadata[1],
-        "t": "event",
-        "ds": "mp - megalista",
-        "cid": row['client_id'],
-        "ea": row['event_action'],
-        "ec": row['event_category'],
-        "ev": row['event_value'],
-        "el": row['event_label'],
-        "ua": self.UA
-      } for row in rows]
-    encoded = [self._format_hit(payload) for payload in payloads]
-    payload = '\n'.join(encoded)
-    response = requests.post(url=self.API_URL, data=payload)
-    if (response.status_code != 200):
-      raise Exception(f"Error uploading to Analytics HTTP {response.status_code}: {response.raw}") 
-    
+    @utils.safe_process(logger=logging.getLogger("megalista.GoogleAnalyticsMeasurementProtocolUploader"))
+    def process(self, elements, **kwargs):
+        any_execution = elements[0]['execution']
+        rows = utils.extract_rows(elements)
+        payloads = [{
+            "v": 1,
+            "tid": any_execution.destination.destination_metadata[0],
+            "ni": any_execution.destination.destination_metadata[1],
+            "t": "event",
+            "ds": "mp - megalista",
+            "cid": row['client_id'],
+            "ea": row['event_action'],
+            "ec": row['event_category'],
+            "ev": row['event_value'],
+            "el": row['event_label'],
+            "ua": self.UA,
+            **{key: row[key] for key in list(filter(lambda key: key.startswith("cd"), row.keys()))}
+        } for row in rows]
+        encoded = [self._format_hit(payload) for payload in payloads]
+        payload = '\n'.join(encoded)
+        response = requests.post(url=self.API_URL, data=payload)
+        if (response.status_code != 200):
+            raise Exception(
+                f"Error uploading to Analytics HTTP {response.status_code}: {response.raw}")
