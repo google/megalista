99175ee7f2ec4eab3fcdd5be7fc114cf45be2570
Antônio Moreira <antoniomoreira@google.com>
2020-05-12 18:21:21 -0300

    Don't fail when general keys are missing from config trix

--- megalist_dataflow/utils/sheets_config.py
+++ megalist_dataflow/utils/sheets_config.py
@@ -45,4 +45,7 @@ class SheetsConfig:
     return self._get_sheets_service().spreadsheets().values().get(spreadsheetId=sheet_id, range=range).execute()
 
   def get_value(self, sheet_id, range):
-    return self.get_range(sheet_id, range)['values'][0][0]
+    range = self.get_range(sheet_id, range)
+    if range.get('values') is None:
+      return None
+    return range['values'][0][0]
