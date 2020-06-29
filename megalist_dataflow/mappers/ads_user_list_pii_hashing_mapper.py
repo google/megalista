ce4e25b17a71d6759431033da2b3bba82eeb0148
Ricardo Godoy <rgodoy@google.com>
2020-06-29 14:14:35 -0300

    Adding flag to skip user data hashing

--- megalist_dataflow/mappers/ads_user_list_pii_hashing_mapper.py
+++ megalist_dataflow/mappers/ads_user_list_pii_hashing_mapper.py
@@ -13,49 +13,79 @@
 # limitations under the License.
 
 
-class AdsUserListPIIHashingMapper():
-  def _hash_field(self, s):
-    import hashlib
-    return hashlib.sha256(s.strip().lower().encode('utf-8')).hexdigest()
+class FieldHasher:
 
-  def _hash_user(self, user):
-    hashed = dict()
-    try:
-      if 'email' in user:
-        hashed['hashedEmail'] = self._hash_field(user['email'])
-    except:
-      print("Error hashing email for user: %s" % user)
+    def __init__(self, should_hash_fields):
+        self.should_hash_fields = should_hash_fields
 
-    try:
-      if 'mailing_address_first_name' in user and 'mailing_address_last_name' in user:
-        hashed['addressInfo'] = {
-          'hashedFirstName': self._hash_field(user['mailing_address_first_name']),
-          'hashedLastName': self._hash_field(user['mailing_address_last_name']),
-          'countryCode': user['mailing_address_country'],
-          'zipCode': user['mailing_address_zip']
-        }
-    except:
-      print("Error hashing address for user: %s" % user)
+    def hash_field(self, field):
+        import hashlib
 
-    try:
-      if 'phone' in user:
-        hashed['hashedPhoneNumber'] = self._hash_field(user['phone'])
-    except:
-      print("Error hashing phone for user: %s" % user)
+        if self.should_hash_fields:
+            return hashlib.sha256(field.strip().lower().encode('utf-8')).hexdigest()
 
-    try:
-      if 'mobile_device_id' in user:
-        hashed['mobileId'] = user['mobile_device_id']
-    except:
-      print("Error hashing mobile_device_id for user: %s" % user)
+        return field
 
-    try:
-      if 'user_id' in user:
-        hashed['userId'] = user['user_id']
-    except:
-      print("Error hashing user_id for user: %s" % user)
 
-    return hashed
+class AdsUserListPIIHashingMapper:
 
-  def hash_users(self, dicts):
-    return [{'execution': dict['execution'], 'row': self._hash_user(dict['row'])} for dict in dicts]
+    def _hash_user(self, user, hasher):
+
+        hashed = dict()
+
+        try:
+            if 'email' in user:
+                hashed['hashedEmail'] = hasher.hash_field(user['email'])
+        except:
+            print("Error hashing email for user: %s" % user)
+
+        try:
+            if 'mailing_address_first_name' in user and 'mailing_address_last_name' in user:
+                hashed['addressInfo'] = {
+                    'hashedFirstName': hasher.hash_field(user['mailing_address_first_name']),
+                    'hashedLastName': hasher.hash_field(user['mailing_address_last_name']),
+                    'countryCode': user['mailing_address_country'],
+                    'zipCode': user['mailing_address_zip']
+                }
+        except:
+            print("Error hashing address for user: %s" % user)
+
+        try:
+            if 'phone' in user:
+                hashed['hashedPhoneNumber'] = hasher.hash_field(user['phone'])
+        except:
+            print("Error hashing phone for user: %s" % user)
+
+        try:
+            if 'mobile_device_id' in user:
+                hashed['mobileId'] = user['mobile_device_id']
+        except:
+            print("Error hashing mobile_device_id for user: %s" % user)
+
+        try:
+            if 'user_id' in user:
+                hashed['userId'] = user['user_id']
+        except:
+            print("Error hashing user_id for user: %s" % user)
+
+        return hashed
+
+    def _get_should_hash_fields(self, metadata_list):
+
+        if len(metadata_list) < 3:
+            return True
+
+        should_hash_fields = metadata_list[2]
+
+        if not should_hash_fields:
+            return True
+
+        return should_hash_fields.lower() != 'false'
+
+    def hash_users(self, dicts):
+
+        should_hash_fields = self._get_should_hash_fields(dicts[0]['execution'].destination.destination_metadata)
+        print('Should hash fields is %s' % should_hash_fields)
+
+        return [{'execution': dict['execution'], 'row': self._hash_user(dict['row'], FieldHasher(should_hash_fields))}
+                for dict in dicts]
--- test/megalist_dataflow/mappers/pii_hashing_mapper_test.py
+++ test/megalist_dataflow/mappers/pii_hashing_mapper_test.py
@@ -15,8 +15,25 @@
 from megalist_dataflow.mappers.ads_user_list_pii_hashing_mapper import AdsUserListPIIHashingMapper
 
 
-def test_pii_hashing():
+def test_get_should_hash_fields():
+
     hasher = AdsUserListPIIHashingMapper()
+
+    # True
+    assert hasher._get_should_hash_fields(['ListName', 'Operator', 'True'])
+    assert hasher._get_should_hash_fields(['ListName', 'Operator'])
+    assert hasher._get_should_hash_fields(['ListName', 'Operator', None])
+    assert hasher._get_should_hash_fields(['ListName', 'Operator', ''])
+    assert hasher._get_should_hash_fields(['ListName', 'Operator', 'anything'])
+
+    # False
+    assert not hasher._get_should_hash_fields(['ListName', 'Operator', 'false'])
+    assert not hasher._get_should_hash_fields(['ListName', 'Operator', 'FALSE'])
+    assert not hasher._get_should_hash_fields(['ListName', 'Operator', 'False'])
+
+
+def test_pii_hashing(mocker):
+
     users = [{
         "email": "john@doe.com",
         "mailing_address_first_name": "John",
@@ -25,15 +42,29 @@ def test_pii_hashing():
         "mailing_address_country": "US"
     },
         {
-        "email": "jane@doe.com",
-        "mailing_address_first_name": "Jane",
-        "mailing_address_last_name": "Doe",
-        "mailing_address_zip": "12345",
-        "mailing_address_country": "US"
-    }]
-    hashed = hasher.hash_users(users)
+            "email": "jane@doe.com",
+            "mailing_address_first_name": "Jane",
+            "mailing_address_last_name": "Doe",
+            "mailing_address_zip": "12345",
+            "mailing_address_country": "US"
+        }]
+
+    # Execution mock
+    execution = mocker.MagicMock()
+    execution.destination.destination_metadata = ['Audience', 'ADD']
+
+    dicts = [
+        {'execution': execution, 'row': users[0]},
+        {'execution': execution, 'row': users[1]}
+    ]
+
+    # Call
+    hasher = AdsUserListPIIHashingMapper()
+    hashed = hasher.hash_users(dicts)
+
     assert len(hashed) == 2
-    assert hashed[0] == {
+
+    assert hashed[0]['row'] == {
         'hashedEmail': 'd709f370e52b57b4eb75f04e2b3422c4d41a05148cad8f81776d94a048fb70af',
         'addressInfo': {
             'countryCode': 'US',
@@ -41,11 +72,62 @@ def test_pii_hashing():
             'hashedLastName': '799ef92a11af918e3fb741df42934f3b568ed2d93ac1df74f1b8d41a27932a6f',
             'zipCode': '12345'
         }}
-    assert hashed[1] == {
-    'hashedEmail': '7c815580ad3844bcb627c74d24eaf700e1a711d9c23e9beb62ab8d28e8cb7954',
-    'addressInfo': {
-        'countryCode': 'US',
-        'hashedFirstName': '81f8f6dde88365f3928796ec7aa53f72820b06db8664f5fe76a7eb13e24546a2',
-        'hashedLastName': '799ef92a11af918e3fb741df42934f3b568ed2d93ac1df74f1b8d41a27932a6f',
-        'zipCode': '12345'
-    }}
+
+    assert hashed[1]['row'] == {
+        'hashedEmail': '7c815580ad3844bcb627c74d24eaf700e1a711d9c23e9beb62ab8d28e8cb7954',
+        'addressInfo': {
+            'countryCode': 'US',
+            'hashedFirstName': '81f8f6dde88365f3928796ec7aa53f72820b06db8664f5fe76a7eb13e24546a2',
+            'hashedLastName': '799ef92a11af918e3fb741df42934f3b568ed2d93ac1df74f1b8d41a27932a6f',
+            'zipCode': '12345'
+        }}
+
+
+def test_avoid_pii_hashing(mocker):
+    users = [{
+        "email": "john@doe.com",
+        "mailing_address_first_name": "John",
+        "mailing_address_last_name": "Doe",
+        "mailing_address_zip": "12345",
+        "mailing_address_country": "US"
+    },
+        {
+            "email": "jane@doe.com",
+            "mailing_address_first_name": "Jane",
+            "mailing_address_last_name": "Doe",
+            "mailing_address_zip": "12345",
+            "mailing_address_country": "US"
+        }]
+
+    # Mock the execution
+    execution = mocker.MagicMock()
+    execution.destination.destination_metadata = ['Audience', 'ADD', 'False']
+
+    dicts = [
+        {'execution': execution, 'row': users[0]},
+        {'execution': execution, 'row': users[1]}
+    ]
+
+    # Call
+    hasher = AdsUserListPIIHashingMapper()
+    hashed = hasher.hash_users(dicts)
+
+    assert len(hashed) == 2
+
+    assert hashed[0]['row'] == {
+        'hashedEmail': 'john@doe.com',
+        'addressInfo': {
+            'countryCode': 'US',
+            'hashedFirstName': 'John',
+            'hashedLastName': 'Doe',
+            'zipCode': '12345'
+        }}
+
+    assert hashed[1]['row'] == {
+        'hashedEmail': 'jane@doe.com',
+        'addressInfo': {
+            'countryCode': 'US',
+            'hashedFirstName': 'Jane',
+            'hashedLastName': 'Doe',
+            'zipCode': '12345'
+        }}
