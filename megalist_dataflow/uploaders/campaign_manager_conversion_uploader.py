b4b1ed9908f10dca6dd713bd9cad01edbf053ee6
Lucas Santos <santosls@google.com>
2020-08-06 10:05:22 -0300

    Refactors campaign_manager_conversion_uploader.py to be in accordance with Google's Python style guide
    Change-Id: I1e61a154b5073d453e87c3a3bf5b5433b9b7e572

--- megalist_dataflow/uploaders/campaign_manager_conversion_uploader.py
+++ megalist_dataflow/uploaders/campaign_manager_conversion_uploader.py
@@ -1,3 +1,4 @@
+"""Campaign Manager Conversion Uploader beam module."""
 # Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,19 +13,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
-import math
 import logging
-from google.oauth2.credentials import Credentials
-from googleapiclient.discovery import build
-import apache_beam as beam
+import math
+import time
 
+import apache_beam as beam
+from googleapiclient.discovery import build
 from uploaders import google_ads_utils as ads_utils
-from uploaders import utils as utils
+from uploaders import utils
 from utils.execution import DestinationType
+
+from google.oauth2.credentials import Credentials
+
+_LOGGER_NAME: str = 'megalista.CampaignManagerConversionsUploader'
 
 
 class CampaignManagerConversionUploaderDoFn(beam.DoFn):
+  """Apache Beam DoFn class implementation."""
+
   def __init__(self, oauth_credentials):
     super().__init__()
     self.oauth_credentials = oauth_credentials
@@ -32,14 +38,15 @@ class CampaignManagerConversionUploaderDoFn(beam.DoFn):
 
   def _get_dcm_service(self):
     credentials = Credentials(
-      token=self.oauth_credentials.get_access_token(),
-      refresh_token=self.oauth_credentials.get_refresh_token(),
-      client_id=self.oauth_credentials.get_client_id(),
-      client_secret=self.oauth_credentials.get_client_secret(),
-      token_uri='https://accounts.google.com/o/oauth2/token',
-      scopes=['https://www.googleapis.com/auth/dfareporting',
-              'https://www.googleapis.com/auth/dfatrafficking',
-              'https://www.googleapis.com/auth/ddmconversions'])
+        token=self.oauth_credentials.get_access_token(),
+        refresh_token=self.oauth_credentials.get_refresh_token(),
+        client_id=self.oauth_credentials.get_client_id(),
+        client_secret=self.oauth_credentials.get_client_secret(),
+        token_uri='https://accounts.google.com/o/oauth2/token',
+        scopes=[
+            'https://www.googleapis.com/auth/dfareporting',
+            'https://www.googleapis.com/auth/dfatrafficking',
+            'https://www.googleapis.com/auth/ddmconversions'])
 
     return build('dfareporting', 'v3.3', credentials=credentials)
 
@@ -50,62 +57,83 @@ class CampaignManagerConversionUploaderDoFn(beam.DoFn):
   def _assert_all_list_names_are_present(any_execution):
     destination = any_execution.destination.destination_metadata
     if len(destination) != 2:
-      raise ValueError('Missing destination information. Found {}'.format(len(destination)))
+      raise ValueError(
+          f'Missing destination information. Found {len(destination)}')
 
     if not destination[0] \
         or not destination[1]:
-      raise ValueError('Missing destination information. Received {}'.format(str(destination)))
+      raise ValueError(
+          f'Missing destination information. Received {str(destination)}')
 
-  @utils.safe_process(logger=logging.getLogger("megalista.CampaignManagerConversionUploader"))
+  @utils.safe_process(
+      logger=logging.getLogger(_LOGGER_NAME))
   def process(self, elements, **kwargs):
     self._do_process(elements, time.time())
 
-  def _do_process(self, elements, time):
+  def _do_process(self, elements, timestamp):
     if not self.active:
-      logging.getLogger("megalista.CampaignManagerConversionsUploader").warning("Skipping upload to Campaign Manager, parameters not configured.")
+      logging.getLogger(_LOGGER_NAME).warning(
+          'Skipping upload to Campaign Manager, parameters not configured.')
       return
 
     ads_utils.assert_elements_have_same_execution(elements)
     any_execution = elements[0]['execution']
-    ads_utils.assert_right_type_action(any_execution, DestinationType.CM_OFFLINE_CONVERSION)
+    ads_utils.assert_right_type_action(
+        any_execution, DestinationType.CM_OFFLINE_CONVERSION)
     self._assert_all_list_names_are_present(any_execution)
 
-    floodlight_activity_id = any_execution.destination.destination_metadata[0]
-    floodlight_configuration_id = any_execution.destination.destination_metadata[1]
+    self._do_upload_data(
+        any_execution.destination.destination_metadata[0],
+        any_execution.destination.destination_metadata[1],
+        any_execution.account_config.campaign_manager_account_id,
+        timestamp,
+        utils.extract_rows(elements))
 
-    self._do_upload_data(floodlight_activity_id, floodlight_configuration_id, any_execution.account_config.campaign_manager_account_id, time, utils.extract_rows(elements))
-
-  def _do_upload_data(self, floodlight_activity_id, floodlight_configuration_id, campaign_manager_account_id, time, rows):
+  def _do_upload_data(
+      self,
+      floodlight_activity_id,
+      floodlight_configuration_id,
+      campaign_manager_account_id,
+      timestamp,
+      rows):
 
     service = self._get_dcm_service()
     conversions = []
     for conversion in rows:
       to_upload = {
-        'floodlightActivityId': floodlight_activity_id,
-        'floodlightConfigurationId': floodlight_configuration_id,
-        'ordinal': math.floor(time * 10e5),
-        'timestampMicros': math.floor(time * 10e5)
+          'floodlightActivityId': floodlight_activity_id,
+          'floodlightConfigurationId': floodlight_configuration_id,
+          'ordinal': math.floor(timestamp * 10e5),
+          'timestampMicros': math.floor(timestamp * 10e5)
       }
-      logging.getLogger("megalista.CampaignManagerConversionsUploader").info(conversion)
+
+      logging.getLogger(_LOGGER_NAME).info(conversion)
+
       if 'gclid' in conversion and conversion['gclid']:
         to_upload['gclid'] = conversion['gclid']
       elif 'encryptedUserId' in conversion and conversion['encryptedUserId']:
         to_upload['encryptedUserId'] = conversion['encryptedUserId']
       elif 'mobileDeviceId' in conversion and conversion['mobileDeviceId']:
         to_upload['mobileDeviceId'] = conversion['mobileDeviceId']
-      logging.getLogger("megalista.CampaignManagerConversionsUploader").info(to_upload)
+
+      logging.getLogger(_LOGGER_NAME).info(to_upload)
       conversions.append(to_upload)
 
     request_body = {
-      'conversions': conversions,
-      'encryptionInfo': 'AD_SERVING'
+        'conversions': conversions,
+        'encryptionInfo': 'AD_SERVING'
     }
-    request = service.conversions().batchinsert(profileId=campaign_manager_account_id, body=request_body)
+
+    request = service.conversions().batchinsert(
+        profileId=campaign_manager_account_id, body=request_body)
     response = request.execute()
+
     if response['hasFailures']:
-      logging.getLogger("megalista.CampaignManagerConversionsUploader").error('Error(s) inserting conversions:')
+      logging.getLogger(_LOGGER_NAME).error('Error(s) inserting conversions:')
       status = response['status'][0]
+
       for error in status['errors']:
-        logging.getLogger("megalista.CampaignManagerConversionsUploader").error('\t[%s]: %s' % (
-          error['code'], error['message']))
-      logging.getLogger("megalista.CampaignManagerConversionsUploader").error(response)
+        logging.getLogger(_LOGGER_NAME).error(
+            '\t[%s]: %s', error['code'], error['message'])
+
+      logging.getLogger(_LOGGER_NAME).error(response)
