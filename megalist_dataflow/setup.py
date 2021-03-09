5e9ee4460a19ec3040278d4ae7107f0b783e0e6a
Álvaro Stivi <astivi@google.com>
2021-03-09 17:33:24 +0000

    Fixes batching for Unbounded collections
    Change-Id: I5eb961df1391a1e610e2303e661c2aa4c613b3cf

--- megalist_dataflow/setup.py
+++ megalist_dataflow/setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     author_email='astivi@google.com',
     url='https://cse.googlesource.com/solutions/megalist',
     install_requires=['googleads==24.1.0', 'google-api-python-client==1.10.0',
-                      'bloom-filter==1.3', 'google-cloud-core==1.3.0', 'google-cloud-bigquery==1.26.0',
+                      'google-cloud-core==1.3.0', 'google-cloud-bigquery==1.26.0',
                       'google-cloud-datastore==1.13.1', 'aiohttp==3.6.2'],
     packages=setuptools.find_packages(),
 )
--- megalist_dataflow/sources/batches_from_executions.py
+++ megalist_dataflow/sources/batches_from_executions.py
@@ -21,73 +21,81 @@ from apache_beam.io.gcp.bigquery import ReadFromBigQueryRequest
 
 from models.execution import DestinationType, Execution, Batch
 
+
 class BatchesFromExecutions(beam.PTransform):
-  """
-  Filter the received executions by the received action,
-  load the data using the received source and group by that batch size and Execution.
-  """
+    """
+    Filter the received executions by the received action,
+    load the data using the received source and group by that batch size and Execution.
+    """
 
-  class _ExecutionIntoBigQueryRequest(beam.DoFn):
-    def process(self, execution: Execution) -> Iterable[ReadFromBigQueryRequest]:
-        table_name = execution.source.source_metadata[0] + '.' + execution.source.source_metadata[1]
-        query = f"SELECT Data.*, '{hash(execution)}' AS execution_hash FROM {table_name} AS Data"
-        return [ReadFromBigQueryRequest(query=query)]
+    class _ExecutionIntoBigQueryRequest(beam.DoFn):
+        def process(self, execution: Execution) -> Iterable[ReadFromBigQueryRequest]:
+            table_name = execution.source.source_metadata[0] + \
+                '.' + execution.source.source_metadata[1]
+            query = f"SELECT Data.*, '{hash(execution)}' AS execution_hash FROM {table_name} AS Data"
+            return [ReadFromBigQueryRequest(query=query)]
 
+    class _ExecutionIntoBigQueryRequestTransactional(beam.DoFn):
+        def process(self, execution: Execution) -> Iterable[ReadFromBigQueryRequest]:
+            table_name = execution.source.source_metadata[0] + \
+                '.' + execution.source.source_metadata[1]
+            uploaded_table_name = f"{table_name}_uploaded"
+            client = bigquery.Client()
 
-  class _ExecutionIntoBigQueryRequestTransactional(beam.DoFn):
-    def process(self, execution: Execution) -> Iterable[ReadFromBigQueryRequest]:
-        table_name = execution.source.source_metadata[0] + '.' + execution.source.source_metadata[1]
-        uploaded_table_name = f"{table_name}_uploaded"
-        client = bigquery.Client()
-
-        query = "CREATE TABLE IF NOT EXISTS " + uploaded_table_name + " ( \
+            query = "CREATE TABLE IF NOT EXISTS " + uploaded_table_name + " ( \
               timestamp TIMESTAMP OPTIONS(description= 'Event timestamp'), \
               uuid STRING OPTIONS(description='Event unique identifier'))\
               PARTITION BY _PARTITIONDATE \
               OPTIONS(partition_expiration_days=15)"
 
-        logging.getLogger("megalista.ExecutionIntoBigQueryRequestTransactional").info(
-          "Creating table %s if it doesn't exist", uploaded_table_name)
+            logging.getLogger("megalista.ExecutionIntoBigQueryRequestTransactional").info(
+                "Creating table %s if it doesn't exist", uploaded_table_name)
 
-        client.query(query).result()
+            client.query(query).result()
 
-        query = f"SELECT Data.*, '{hash(execution)}' AS execution_hash FROM {table_name} AS Data \
+            query = f"SELECT Data.*, '{hash(execution)}' AS execution_hash FROM {table_name} AS Data \
                 LEFT JOIN {uploaded_table_name} AS Uploaded USING(uuid) \
                 WHERE Uploaded.uuid IS NULL;"
 
-        return [ReadFromBigQueryRequest(query=query)]
+            return [ReadFromBigQueryRequest(query=query)]
 
-  class _BatchElements(beam.DoFn):
-    def __init__(self, batch_size: int):
+    class _BatchElements(beam.DoFn):
+        def __init__(self, batch_size: int):
+            self._batch_size = batch_size
+
+        def process(self, element, executions: Iterable[Execution]):
+            execution = next(
+                (execution for execution in executions if str(hash(execution)) == element[0]))
+            batch = []
+            for i, element in enumerate(element[1]):
+                if i != 0 and i % self._batch_size == 0:
+                    yield Batch(execution, batch)
+                    batch = []
+                batch.append(element)
+            yield Batch(execution, batch)
+
+    def __init__(
+        self,
+        destination_type: DestinationType,
+        batch_size: int = 5000,
+        transactional: bool = False
+    ):
+        super().__init__()
+        self._destination_type = destination_type
         self._batch_size = batch_size
+        self._transactional = transactional
 
-    def process(self, element, executions: Iterable[Execution]):
-        execution = next((execution for execution in executions if str(hash(execution)) == element[0]))
-        for i in range(0, len(element[1]), self._batch_size):
-            yield Batch(execution, element[1][i:i+self._batch_size])
+    def _get_bq_request_class(self):
+        if self._transactional:
+            return self._ExecutionIntoBigQueryRequestTransactional()
+        return self._ExecutionIntoBigQueryRequest()
 
-  def __init__(
-      self,
-      destination_type: DestinationType,
-      batch_size: int = 5000,
-      transactional: bool = False
-  ):
-    super().__init__()
-    self._destination_type = destination_type
-    self._batch_size = batch_size
-    self._transactional = transactional
-
-  def _get_bq_request_class(self):
-    if self._transactional:
-      return self._ExecutionIntoBigQueryRequestTransactional()
-    return self._ExecutionIntoBigQueryRequest()
-
-  def expand(self, executions):
-    return (
-        executions
-        | beam.Filter(lambda execution: execution.destination.destination_type == self._destination_type)
-        | beam.ParDo(self._get_bq_request_class())
-        | beam.io.ReadAllFromBigQuery()
-        | beam.GroupBy(lambda x: x['execution_hash'])
-        | beam.ParDo(self._BatchElements(self._batch_size), beam.pvalue.AsList(executions))
-    )
+    def expand(self, executions):
+        return (
+            executions
+            | beam.Filter(lambda execution: execution.destination.destination_type == self._destination_type)
+            | beam.ParDo(self._get_bq_request_class())
+            | beam.io.ReadAllFromBigQuery()
+            | beam.GroupBy(lambda x: x['execution_hash'])
+            | beam.ParDo(self._BatchElements(self._batch_size), beam.pvalue.AsList(executions))
+        )
