92e732285cbcadfa51341a3e5b4ab3f98cdab040
Nivaldo Humberto Oliveira Tokuda <nivaldo.humbertoo@gmail.com>
2021-04-27 17:01:46 -0300

    Add Firestore source

--- megalist_dataflow/models/options.py
+++ megalist_dataflow/models/options.py
@@ -38,6 +38,8 @@ class DataflowOptions(PipelineOptions):
     parser.add_value_provider_argument(
         '--setup_firestore_collection', help='Name of Google Cloud Firestore collection with execution info (don\'t set if using Sheet or JSON)')
     parser.add_value_provider_argument(
+        '--setup_firestore_collection', help='Name of Firestore collection with execution info')
+    parser.add_value_provider_argument(
         '--bq_ops_dataset',
         help='Auxliary bigquery dataset used for Megalista operations')
     # Google Ads
