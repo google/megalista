5bb0dc13fcf335dd1afdcaeac0ab0e344eec793b
antoniolmm <33185202+antoniolmm@users.noreply.github.com>
2021-07-14 18:40:32 -0300

    Update main.tf
    Specifying scopes on "google" terraform provider.
    These scopes are exactly the default ones, according to the documentation, but the deployment was failing without this configuration by missing the scope "https://www.googleapis.com/auth/userinfo.email".

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+provider "google" {
+    scopes = [
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/compute",
+        "https://www.googleapis.com/auth/cloud-platform",
+        "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
+        "https://www.googleapis.com/auth/devstorage.full_control"
+    ]
+}
+
 data "google_client_config" "current" {
 }
 
