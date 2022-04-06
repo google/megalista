a13c72616fc6e6618712a4fda8012e41a46922b0
Antônio Moreira <antoniomoreira@google.com>
2022-04-06 11:30:33 -0300

    Cleanup command line options
    Change-Id: I72c08b65804c546d25eb196016c8349a955fb9d7

--- megalista_dataflow/models/options.py
+++ megalista_dataflow/models/options.py
@@ -43,44 +43,6 @@ class DataflowOptions(PipelineOptions):
     # Google Ads
     parser.add_value_provider_argument(
         '--developer_token', help='Developer Token for Google Ads API')
-    parser.add_value_provider_argument(
-        '--customer_id', help='Google Ads Customer Id')
-    # Google Analytics
-    parser.add_value_provider_argument(
-        '--google_analytics_account_id', help='Google Analytics account Id')
-    parser.add_value_provider_argument(
-        '--google_analytics_web_property_id',
-        help='Google Analytics web property Id')
-    parser.add_value_provider_argument(
-        '--google_analytics_buyer_custom_dim',
-        help='Google Analytics buyer custom dimension')
-    parser.add_value_provider_argument(
-        '--google_analytics_user_id_custom_dim',
-        help='Google Analytics User Id custom dimension')
-    # Campaign Manager
-    parser.add_value_provider_argument(
-        '--dcm_profile_id', help='CampaignManager profile Id')
-    parser.add_value_provider_argument(
-        '--floodlight_activity_id',
-        help='CampaignManager floodlight activity Id')
-    parser.add_value_provider_argument(
-        '--floodlight_configuration_id',
-        help='CampaignManager floodlight configuration Id')
-    # Conversion Plus
-    parser.add_value_provider_argument(
-        '--cp_sheet_id', help='Conversion Plus Sheet Id')
-    parser.add_value_provider_argument(
-        '--cp_sheet_range',
-        help='Name of the Conversion Plus Sheet config range')
-    # BigQuery
-    parser.add_value_provider_argument(
-        '--dataset_id', default='megalist', help='BigQuery dataset Id')
-    parser.add_value_provider_argument(
-        '--table_id', default='crm_upload', help='BigQuery dataset Id')
-    # GCP
-    parser.add_argument(
-        '--gcp_project_id', help='ID Google Cloud Project to use')
-    parser.add_argument('--output', help='Output file to write results to.')
     # APPSFLYER
     parser.add_value_provider_argument(
         '--appsflyer_dev_key', help='Developer key for AppsFlyer S2S API')
