16ee80a16fbb8434c15161c6cc11a7f08e773582
Diogo Aihara <diogoaihara@google.com>
2022-08-01 21:41:47 +0000

    change - insertion into code

--- deployment/deploy_cloud.sh
+++ deployment/deploy_cloud.sh
@@ -27,10 +27,10 @@ echo "Build Dataflow metadata"
 python3 -m pip install --user -q -r requirements.txt
 echo $4
 python3 -m main --runner DataflowRunner --project "$1" --gcp_project_id "$1" --temp_location "gs://$2/tmp/" --region "$3" --setup_file ./setup.py --template_location "gs://$2/templates/megalista" --num_workers 1 --autoscaling_algorithm=NONE --service_account_email "$4"
+echo "Update commit info inside code"
+sed -i "s/\[megalista_version\]/$(git rev-parse HEAD)/" version.py
 echo "Copy megalista_medata to bucket $2"
 gsutil cp megalista_metadata "gs://$2/templates/megalista_metadata"
-echo "Get last commit hash and write into bucket $2"
-git rev-parse HEAD > last-commit.txt
-gsutil cp last-commit.txt "gs://$2/last-commit.txt"
-rm last-commit.txt
+echo "Cleanup"
+sed -i "s/$(git rev-parse HEAD)/\[megalista_version\]/" version.py
 cd ..
