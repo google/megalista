e302463f5b904145af0828dcfaf211f2592e5549
Diogo Aihara <diogoaihara@google.com>
2022-09-02 11:44:12 -0300

    Docker adjustments (GCP authentication using service account json file)

--- deployment/docker/entrypoint.sh
+++ deployment/docker/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Environment variables handling"
+
 ENV_VARS=`env | grep ^MEGALISTA_.*`
 params=""
 
@@ -14,8 +16,11 @@ do
     IFS=' '
 done
 
-echo "Running Megalista"
+export GOOGLE_APPLICATION_CREDENTIALS=/app/megalista_dataflow/service-account-file.json
+
+echo "Activating virual environment (python)"
 source virtual_env/bin/activate
+echo "Running Megalista"
 python megalista_dataflow/main.py \
     --runner DirectRunner \
     --direct_num_workers 0 \
