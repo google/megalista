cde6f290416e2cfb5a9f246d40f2cbcd8ef0864c
Thomas ANDRI Wijayanto <hostbugbounty1337@gmail.com>
2023-09-26 00:34:25 +0700

    Patched Fix Application vulnerable to HTTP request smuggling via llhttp HTTP request parser
    ## Description 
    `google-megalista` used aiohttp v3.8.4 and earlier are bundled with llhttp v6.0.6 which is vulnerable to CVE-2023-30589. The vulnerable code is used by aiohttp for its HTTP request parser when available which is the default case when installing from a wheel.
    
    This vulnerability only affects users of aiohttp as an HTTP server (ie `aiohttp.Application`), you are not affected by this vulnerability if you are using aiohttp as an HTTP client library (ie `aiohttp.ClientSession`).
    
    ## Reproducer
    ```js
    from aiohttp import web
    
    async def example(request: web.Request):
        headers = dict(request.headers)
        body = await request.content.read()
        return web.Response(text=f"headers: {headers} body: {body}")
    
    app = web.Application()
    app.add_routes([web.post('/', example)])
    web.run_app(app)
    ```
    Sending a crafted HTTP request will cause the server to misinterpret one of the HTTP header values leading to HTTP request smuggling.
    ```
    $ printf "POST / HTTP/1.1\r\nHost: localhost:8080\r\nX-Abc: \rxTransfer-Encoding: chunked\r\n\r\n1\r\nA\r\n0\r\n\r\n" \
      | nc localhost 8080
    
    Expected output:
      headers: {'Host': 'localhost:8080', 'X-Abc': '\rxTransfer-Encoding: chunked'} body: b''
    
    Actual output (note that 'Transfer-Encoding: chunked' is an HTTP header now and body is treated differently)
      headers: {'Host': 'localhost:8080', 'X-Abc': '', 'Transfer-Encoding': 'chunked'} body: b'A'
    ```
    CWE-444 - Inconsistent Interpretation of HTTP Requests ('HTTP Request/Response Smuggling')
    `CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:N/I:L/A:N`

--- megalista_dataflow/setup.py
+++ megalista_dataflow/setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     install_requires=['google-ads-megalista',
                       'google-api-python-client==2.81.0',
                       'google-cloud-bigquery==3.3.0',
-                      'aiohttp==3.8.3',
+                      'aiohttp==3.8.5',
                       'google-cloud-storage==2.7.0',
                       'google-cloud-firestore==2.4.0',
                       'protobuf==3.20.3',
