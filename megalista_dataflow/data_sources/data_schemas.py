997b08d5915929946bde341a43c824cc3698c6a9
joaoaleixogoogle <joal@google.com>
2023-10-25 17:14:20 +0000

    fixing commas be missing

--- megalista_dataflow/data_sources/data_schemas.py
+++ megalista_dataflow/data_sources/data_schemas.py
@@ -49,8 +49,8 @@ _dtypes: Dict[str, Dict[str, Any]] = {
         'columns': [
             {'name': 'gclid', 'required': True, 'data_type': 'string'},
             {'name': 'time', 'required': True, 'data_type': 'string'},
-            {'name': 'amount', 'required': True, 'data_type': 'string'}
-            {'name': 'external_attribution_credit', 'required': False, 'data_type': 'string'}
+            {'name': 'amount', 'required': True, 'data_type': 'string'},
+            {'name': 'external_attribution_credit', 'required': False, 'data_type': 'string'},
             {'name': 'external_attribution_model', 'required': False, 'data_type': 'string'}
         ],
         'groups': []
