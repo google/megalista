a0293db56a8bc25e0fe02c3505bbf5f1322a74e0
Pedro Lopes <6962758+Mr-Lopes@users.noreply.github.com>
2023-11-06 20:29:24 +0000

    Adds SSI connector

---
+++ megalista_dataflow/uploaders/google_ads/conversions/google_ads_ssi_uploader.py
@@ -0,0 +1,190 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+import logging
+from typing import Optional
+
+from error.error_handling import ErrorHandler
+from models.execution import AccountConfig, Batch, Destination, Execution
+from uploaders import utils
+from utils.utils import Utils
+from uploaders.google_ads import ADS_API_VERSION
+from uploaders.uploaders import MegalistaUploader
+
+
+class GoogleAdsSSIUploaderDoFn(MegalistaUploader):
+
+    def __init__(self, oauth_credentials, developer_token, error_handler: ErrorHandler):
+        super().__init__(error_handler)
+        self.oauth_credentials = oauth_credentials
+        self.developer_token = developer_token
+        self.active = developer_token is not None
+
+    def _get_offline_user_data_job_service(self, customer_id):
+        return utils.get_ads_service('OfflineUserDataJobService', ADS_API_VERSION,
+                                         self.oauth_credentials,
+                                         self.developer_token.get(),
+                                         customer_id)
+
+    @staticmethod
+    def _assert_conversion_metadata_is_present(execution: Execution):
+        metadata = execution.destination.destination_metadata
+        if len(metadata) < 3:
+            raise ValueError(
+                f'Missing destination information. Received {len(metadata)} entry(ies)')
+
+    @utils.safe_process(
+        logger=logging.getLogger('megalista.GoogleAdsSSIUploader'))
+    def process(self, batch: Batch, **kwargs):
+        execution = batch.execution
+        self._assert_conversion_metadata_is_present(execution)
+
+        currency_code = self._get_currency_code(execution.destination)
+        customer_id = self._get_customer_id(execution.account_config, execution.destination)
+        login_customer_id = self._get_login_customer_id(execution.account_config, execution.destination)
+        user_data_consent = self._get_user_data_consent(execution.account_config, execution.destination)
+        ad_personalization = self._get_ad_personalization_consent(execution.account_config, execution.destination)
+
+        offline_user_data_job_service = self._get_offline_user_data_job_service(
+            login_customer_id)
+        conversion_action_resource_name = self._get_resource_name(customer_id,
+                                                                  login_customer_id,  
+                                                                  execution.destination.destination_metadata[0])
+        self._do_upload(execution,
+                        offline_user_data_job_service,
+                        customer_id,
+                        currency_code,
+                        conversion_action_resource_name,
+                        user_data_consent,
+                        ad_personalization,
+                        batch.elements)
+
+        return [execution]
+
+    def _do_upload(self, execution, offline_user_data_job_service, customer_id, currency_code, conversion_action_resource_name, user_data_consent, ad_personalization, rows):
+        logger = logging.getLogger('megalista.GoogleAdsSSIUploader')
+
+        # Upload is divided into 3 parts:
+        # 1. Creates Job
+        # 2. Creates operations (data insertion)
+        # 3. Runs the Job
+
+        # 1. Creates Job
+        job_creation_payload = {
+            'type': 'STORE_SALES_UPLOAD_FIRST_PARTY',
+            'external_id':  int(datetime.datetime.now().timestamp()*10e3),
+            'store_sales_metadata': {
+                'loyalty_fraction': 1.0,
+                'transaction_upload_fraction': 1.0
+            }
+        }
+
+        job_resource_name = offline_user_data_job_service.create_offline_user_data_job(customer_id = customer_id, job = job_creation_payload).resource_name
+        
+        # Sets consent info if any
+        consent = {}
+        if user_data_consent:
+            consent['ad_user_data'] = user_data_consent
+        if  ad_personalization:
+            consent['ad_personalization'] = ad_personalization
+
+        # 2. Creates operations (data insertion)
+        data_insertion_payload = {
+            'resource_name': job_resource_name,
+            'enable_partial_failure': False,
+            'operations': [{
+                'create': {
+                    'user_identifiers': [{k: v} for (k, v) in conversion.items() if k not in ('amount', 'time')],
+                    'transaction_attribute': {
+                        'conversion_action': conversion_action_resource_name,
+                        'currency_code': currency_code,
+                        'transaction_amount_micros': conversion['amount'],
+                        'transaction_date_time': utils.format_date(conversion['time'])
+                    },
+                    **({'consent':consent} if consent else {})
+                }
+            } for conversion in rows]
+        }
+
+        data_insertion_response = offline_user_data_job_service.add_offline_user_data_job_operations(request = data_insertion_payload)
+
+        error_message = utils.print_partial_error_messages(logger, 'uploading ssi',
+                                                           data_insertion_response)
+        if error_message:
+            self._add_error(execution, error_message)
+
+        # 3. Runs the Job
+        offline_user_data_job_service.run_offline_user_data_job(resource_name = job_resource_name)
+
+    def _get_currency_code(self, destination: Destination) -> str:
+        """
+          If the currency_code is present on the destination, return it, otherwise default to BRL.
+        """
+        if len(destination.destination_metadata) >= 5 and len(destination.destination_metadata[3]) > 0:
+            return destination.destination_metadata[3]
+        return 'BRL'
+
+    def _get_customer_id(self, account_config: AccountConfig, destination: Destination) -> str:
+        """
+          If the customer_id is present on the destination, returns it, otherwise defaults to the account_config info.
+        """
+        if len(destination.destination_metadata) >= 5 and len(destination.destination_metadata[4]) > 0:
+            return Utils.filter_text_only_numbers(destination.destination_metadata[4])
+        return account_config.google_ads_account_id
+    
+    def _get_user_data_consent(self, account_config: AccountConfig, destination: Destination) -> Optional[str]:
+        """
+          Specifies whether user consent was obtained for the data you are uploading.
+          https://www.google.com/about/company/user-consent-policy
+        """
+        if len(destination.destination_metadata) >= 6 and len(destination.destination_metadata[5]) > 0:
+            return destination.destination_metadata[5]
+        return None
+    
+    def _get_ad_personalization_consent(self, account_config: AccountConfig, destination: Destination) -> Optional[str]:
+        """
+          Specifies whether user consent was obtained for the data you are uploading.
+          https://www.google.com/about/company/user-consent-policy
+        """
+        if len(destination.destination_metadata) >= 6 and len(destination.destination_metadata[6]) > 0:
+            return destination.destination_metadata[6]
+        return None
+
+    def _get_login_customer_id(self, account_config: AccountConfig, destination: Destination) -> str:
+        """
+          If the customer_id in account_config is a mcc, then login with the mcc account id, otherwise use the customer id.
+        """
+        if account_config._mcc:
+            return account_config.google_ads_account_id
+        
+        return self._get_customer_id(account_config, destination)
+
+    def _get_ads_service(self, customer_id: str):
+      return utils.get_ads_service('GoogleAdsService', ADS_API_VERSION,
+                                       self.oauth_credentials,
+                                       self.developer_token.get(),
+                                       customer_id)
+
+    def _get_resource_name(self, customer_id: str, login_customer_id: str, name: str):
+        resource_name = None
+        service = self._get_ads_service(login_customer_id)
+        query = f"SELECT conversion_action.resource_name FROM conversion_action WHERE conversion_action.name = '{name}'"
+        response_query = service.search_stream(customer_id=customer_id, query=query)
+        for batch in response_query:
+          for row in batch.results:
+            resource_name = row.conversion_action.resource_name
+        return resource_name
+
+
---
+++ megalista_dataflow/uploaders/google_ads/conversions/google_ads_ssi_uploader_test.py
@@ -0,0 +1,279 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+from apache_beam.options.value_provider import StaticValueProvider
+
+from error.error_handling import ErrorHandler
+from error.error_handling_test import MockErrorNotifier
+from models.execution import AccountConfig
+from models.execution import Batch
+from models.execution import Destination
+from models.execution import DestinationType
+from models.execution import Execution
+from models.execution import Source
+from models.execution import SourceType
+from models.oauth_credentials import OAuthCredentials
+from uploaders.google_ads.conversions.google_ads_ssi_uploader import GoogleAdsSSIUploaderDoFn
+
+_ads_account_id = '1234567890'
+_ads_mcc_account_id = '0987654321'
+_ads_account_override = '12121212121'
+_ga_account_id = '3456789'
+
+_account_config = AccountConfig(_ads_account_id, False, _ga_account_id, '', '')
+_mcc_account_config = AccountConfig(_ads_mcc_account_id, True, _ga_account_id, '', '')
+
+_conversion_name = 'ssi_conversion'
+_external_upload_id = '123'
+_should_hash = True
+
+_time1 = '2020-04-09T14:13:55.0005'
+_time1_result = '2020-04-09 14:13:55-03:00'
+
+_time2 = '2020-04-09T13:13:55.0005'
+_time2_result = '2020-04-09 13:13:55-03:00'
+
+@pytest.fixture
+def uploader(mocker):
+    mocker.patch('google.ads.googleads.client.GoogleAdsClient')
+    mocker.patch('google.ads.googleads.oauth2')
+    id = StaticValueProvider(str, 'id')
+    secret = StaticValueProvider(str, 'secret')
+    access = StaticValueProvider(str, 'access')
+    refresh = StaticValueProvider(str, 'refresh')
+    credentials = OAuthCredentials(id, secret, access, refresh)
+    return GoogleAdsSSIUploaderDoFn(credentials,
+                                    StaticValueProvider(str, 'devtoken'),
+                                    ErrorHandler(DestinationType.ADS_SSI_UPLOAD, MockErrorNotifier()))
+
+@pytest.fixture
+def ssi_batch():
+    return create_batch(_account_config, '', '', '', '')
+
+@pytest.fixture
+def ssi_batch_with_account_and_currency_override():
+    return create_batch(_account_config, 'currency_override', _ads_account_override, '', '')
+
+@pytest.fixture
+def ssi_batch_with_mcc_account_override():
+    return create_batch(_mcc_account_config, '', '', '', '')
+
+@pytest.fixture
+def ssi_batch_with_consent_override():
+    return create_batch(_account_config, '', '', 'GRANTED', 'GRANTED')
+
+def create_batch(account_config, currency_override, account_override, user_data_consent, ad_personalization):
+    source = Source('orig1', SourceType.BIG_QUERY, ('dt1', 'buyers'))
+    destination = Destination('dest1', DestinationType.ADS_SSI_UPLOAD,
+                              [_conversion_name, _external_upload_id, _should_hash, currency_override, account_override, user_data_consent, ad_personalization])
+    execution = Execution(account_config, source, destination)
+
+    return Batch(execution, [{
+        'hashed_email': 'a@a.com',
+        'time': _time1,
+        'amount': '123'
+    }, {
+        'hashed_email': 'b@b.com',
+        'time': _time2,
+        'amount': '234'
+    }])
+
+def test_get_service(mocker, uploader):
+    assert uploader._get_offline_user_data_job_service(mocker.ANY) is not None
+
+def test_fail_missing_destination_metadata(uploader, mocker):
+    mocker.patch.object(uploader, '_get_offline_user_data_job_service')
+    source = Source('orig1', SourceType.BIG_QUERY, ('dt1', 'buyers'))
+    destination = Destination('dest1', DestinationType.ADS_SSI_UPLOAD, ['1', '2'])
+    execution = Execution(_account_config, source, destination)
+    batch = Batch(execution, [])
+    uploader.process(batch)
+    uploader._get_offline_user_data_job_service.assert_not_called()
+
+def test_conversion_upload(mocker, uploader, ssi_batch):
+    mocker.patch.object(uploader, '_get_offline_user_data_job_service')
+    mocker.patch.object(uploader, '_get_resource_name')
+
+    resource_name = uploader._get_offline_user_data_job_service.return_value.create_offline_user_data_job.return_value.resource_name
+    conversion_name_resource_name = uploader._get_resource_name.return_value
+
+    uploader.process(ssi_batch)
+
+    data_insertion_payload = {
+        'resource_name': resource_name,
+        'enable_partial_failure': False,
+        'operations': [{
+            'create': {
+                'user_identifiers': [{
+                    'hashed_email': 'a@a.com'
+                }],
+                'transaction_attribute': {
+                    'conversion_action': conversion_name_resource_name,
+                    'currency_code': 'BRL',
+                    'transaction_amount_micros': '123',
+                    'transaction_date_time': _time1_result
+                }
+            }
+        }, {
+            'create': {
+                'user_identifiers': [{
+                    'hashed_email': 'b@b.com'
+                }],
+                'transaction_attribute': {
+                    'conversion_action': conversion_name_resource_name,
+                    'currency_code': 'BRL',
+                    'transaction_amount_micros': '234',
+                    'transaction_date_time': _time2_result
+                }
+            }
+        }]
+    }
+
+    uploader._get_offline_user_data_job_service.assert_called_with(_ads_account_id)
+    uploader._get_offline_user_data_job_service.return_value.add_offline_user_data_job_operations.assert_any_call(request = data_insertion_payload)
+
+def test_conversion_upload_account_and_currency_override(mocker, uploader, ssi_batch_with_account_and_currency_override):
+    mocker.patch.object(uploader, '_get_offline_user_data_job_service')
+    mocker.patch.object(uploader, '_get_resource_name')
+
+    resource_name = uploader._get_offline_user_data_job_service.return_value.create_offline_user_data_job.return_value.resource_name
+    conversion_name_resource_name = uploader._get_resource_name.return_value
+
+    uploader.process(ssi_batch_with_account_and_currency_override)
+
+    data_insertion_payload = {
+        'resource_name': resource_name,
+        'enable_partial_failure': False,
+        'operations': [{
+            'create': {
+                'user_identifiers': [{
+                    'hashed_email': 'a@a.com'
+                }],
+                'transaction_attribute': {
+                    'conversion_action': conversion_name_resource_name,
+                    'currency_code': 'currency_override',
+                    'transaction_amount_micros': '123',
+                    'transaction_date_time': _time1_result
+                }
+            }
+        }, {
+            'create': {
+                'user_identifiers': [{
+                    'hashed_email': 'b@b.com'
+                }],
+                'transaction_attribute': {
+                    'conversion_action': conversion_name_resource_name,
+                    'currency_code': 'currency_override',
+                    'transaction_amount_micros': '234',
+                    'transaction_date_time': _time2_result
+                }
+            }
+        }]
+    }
+
+    uploader._get_offline_user_data_job_service.assert_called_with(_ads_account_override)
+    uploader._get_offline_user_data_job_service.return_value.add_offline_user_data_job_operations.assert_any_call(request = data_insertion_payload)
+
+def test_conversion_mcc_account_override(mocker, uploader, ssi_batch_with_mcc_account_override):
+    mocker.patch.object(uploader, '_get_offline_user_data_job_service')
+    mocker.patch.object(uploader, '_get_resource_name')
+
+    resource_name = uploader._get_offline_user_data_job_service.return_value.create_offline_user_data_job.return_value.resource_name
+    conversion_name_resource_name = uploader._get_resource_name.return_value
+
+    uploader.process(ssi_batch_with_mcc_account_override)
+
+    data_insertion_payload = {
+        'resource_name': resource_name,
+        'enable_partial_failure': False,
+        'operations': [{
+            'create': {
+                'user_identifiers': [{
+                    'hashed_email': 'a@a.com'
+                }],
+                'transaction_attribute': {
+                    'conversion_action': conversion_name_resource_name,
+                    'currency_code': 'BRL',
+                    'transaction_amount_micros': '123',
+                    'transaction_date_time': _time1_result
+                }
+            }
+        }, {
+            'create': {
+                'user_identifiers': [{
+                    'hashed_email': 'b@b.com'
+                }],
+                'transaction_attribute': {
+                    'conversion_action': conversion_name_resource_name,
+                    'currency_code': 'BRL',
+                    'transaction_amount_micros': '234',
+                    'transaction_date_time': _time2_result
+                }
+            }
+        }]
+    }
+
+    uploader._get_offline_user_data_job_service.assert_called_with(_ads_mcc_account_id)
+    uploader._get_offline_user_data_job_service.return_value.add_offline_user_data_job_operations.assert_any_call(request = data_insertion_payload)
+
+def test_conversion_upload_with_consent(mocker, uploader, ssi_batch_with_consent_override):
+    mocker.patch.object(uploader, '_get_offline_user_data_job_service')
+    mocker.patch.object(uploader, '_get_resource_name')
+
+    resource_name = uploader._get_offline_user_data_job_service.return_value.create_offline_user_data_job.return_value.resource_name
+    conversion_name_resource_name = uploader._get_resource_name.return_value
+
+    uploader.process(ssi_batch_with_consent_override)
+
+    data_insertion_payload = {
+        'resource_name': resource_name,
+        'enable_partial_failure': False,
+        'operations': [{
+            'create': {
+                'user_identifiers': [{
+                    'hashed_email': 'a@a.com'
+                }],
+                'transaction_attribute': {
+                    'conversion_action': conversion_name_resource_name,
+                    'currency_code': 'BRL',
+                    'transaction_amount_micros': '123',
+                    'transaction_date_time': _time1_result
+                },
+                'consent': {
+                    'ad_user_data': 'GRANTED',
+                    'ad_personalization': 'GRANTED'
+                }
+            }
+        }, {
+            'create': {
+                'user_identifiers': [{
+                    'hashed_email': 'b@b.com'
+                }],
+                'transaction_attribute': {
+                    'conversion_action': conversion_name_resource_name,
+                    'currency_code': 'BRL',
+                    'transaction_amount_micros': '234',
+                    'transaction_date_time': _time2_result
+                },
+                'consent': {
+                    'ad_user_data': 'GRANTED',
+                    'ad_personalization': 'GRANTED'
+                }
+            }
+        }]
+    }
+
+    uploader._get_offline_user_data_job_service.assert_called_with(_ads_account_id)
+    uploader._get_offline_user_data_job_service.return_value.add_offline_user_data_job_operations.assert_any_call(request = data_insertion_payload)
