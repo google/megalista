8ff698339ea8ad30d686aa0863d9885f8f1656e1
lauraxavierp <laura.pereira@petlove.com.br>
2023-11-30 19:16:35 -0300

    feat: alterando logs para GA4

--- megalista_dataflow/uploaders/google_analytics/google_analytics_4_measurement_protocol.py
+++ megalista_dataflow/uploaders/google_analytics/google_analytics_4_measurement_protocol.py
@@ -70,14 +70,14 @@ class GoogleAnalytics4MeasurementProtocolUploaderDoFn(MegalistaUploader):
     # logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').info(f"[PETLOVE] non_personalized_ads: {non_personalized_ads}")
 
 
-    # Petlove
-    def str_to_timestamp_micros(sheet_date):
-      try: 
-        data = datetime.strptime(sheet_date, "%Y-%m-%d %H:%M:%S.%f")
-        return int(data.timestamp()) * 1000000
+    # # Petlove
+    # def str_to_timestamp_micros(sheet_date):
+    #   try: 
+    #     data = datetime.strptime(sheet_date, "%Y-%m-%d %H:%M:%S.%f")
+    #     return int(data.timestamp()) * 1000000
       
-      except Exception as err:
-        raise ValueError(f'Missing start date information. Received: {sheet_date}. Exception: {err}')
+    #   except Exception as err:
+    #     raise ValueError(f'Missing start date information. Received: {sheet_date}. Exception: {err}')
       
       
 
@@ -87,7 +87,11 @@ class GoogleAnalytics4MeasurementProtocolUploaderDoFn(MegalistaUploader):
 
     # Petlove
     conversion_name = None
+
+    measurement_id = None
     if len(execution.destination.destination_metadata) >= 6:
+      #measurement_id = execution.destination.destination_metadata[5]
+ 
       #logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').info(f"[PETLOVE] execution.destination.destination_metadata: {execution.destination.destination_metadata}")
       conversion_name = execution.destination.destination_metadata[5]
 
@@ -95,43 +99,43 @@ class GoogleAnalytics4MeasurementProtocolUploaderDoFn(MegalistaUploader):
   # Caso esteja como "YYYY-MM-DD HH:mm:SS.fff" irá retornar os últimos 3d; Caso esteja como uma data seguindo o padrão
   # "YYYY-MM-DD HH:mm:SS.fff" irá consultar dessa data
 
-    start_date = str()
-    destination_metadata_start_date = execution.destination.destination_metadata[6]
+    # start_date = str()
+    # destination_metadata_start_date = execution.destination.destination_metadata[6]
     
-    if destination_metadata_start_date  == "YYYY-MM-DD HH:mm:SS.fff":
-      today = datetime.now()
-      unix_today = int(datetime(today.year, today.month, today.day, 23, 59, 59).timestamp())
-      unix_three_days = 86400 * 3
-      start_date = int((unix_today - unix_three_days) * 1000000)
+    # if destination_metadata_start_date  == "YYYY-MM-DD HH:mm:SS.fff":
+    #   today = datetime.now()
+    #   unix_today = int(datetime(today.year, today.month, today.day, 23, 59, 59).timestamp())
+    #   unix_three_days = 86400 * 3
+    #   start_date = int((unix_today - unix_three_days) * 1000000)
      
-    elif destination_metadata_start_date != "YYYY-MM-DD HH:mm:SS.fff":
-      try:
-        start_date = str_to_timestamp_micros(destination_metadata_start_date) 
+    # elif destination_metadata_start_date != "YYYY-MM-DD HH:mm:SS.fff":
+    #   try:
+    #     start_date = str_to_timestamp_micros(destination_metadata_start_date) 
      
-      except Exception as error:
-          raise ValueError(f'Invalid format date of start_date. Received: {destination_metadata_start_date}. Expected: \"YYYY-MM-DD HH:mm:SS.fff\" | \n error: {error}')
+    #   except Exception as error:
+    #       raise ValueError(f'Invalid format date of start_date. Received: {destination_metadata_start_date}. Expected: \"YYYY-MM-DD HH:mm:SS.fff\" | \n error: {error}')
 
   
-    stop_date = str()
-    destination_metadata_stop_date = execution.destination.destination_metadata[7]
+    # stop_date = str()
+    # destination_metadata_stop_date = execution.destination.destination_metadata[7]
     
-    if destination_metadata_stop_date == "YYYY-MM-DD HH:mm:SS.fff":
-      today = datetime.now()
-      unix_today = int(datetime(today.year, today.month, today.day, 23, 59, 59).timestamp())
-      unix_one_day = 86400
-      stop_date = int((unix_today - unix_one_day) * 1000000)
+    # if destination_metadata_stop_date == "YYYY-MM-DD HH:mm:SS.fff":
+    #   today = datetime.now()
+    #   unix_today = int(datetime(today.year, today.month, today.day, 23, 59, 59).timestamp())
+    #   unix_one_day = 86400
+    #   stop_date = int((unix_today - unix_one_day) * 1000000)
 
-    elif destination_metadata_stop_date != "YYYY-MM-DD HH:mm:SS.fff":
-      try:
-        stop_date = str_to_timestamp_micros(destination_metadata_stop_date)
+    # elif destination_metadata_stop_date != "YYYY-MM-DD HH:mm:SS.fff":
+    #   try:
+    #     stop_date = str_to_timestamp_micros(destination_metadata_stop_date)
       
-      except Exception as error:
-          raise ValueError(f'Invalid format date of stop_date. Received: {destination_metadata_stop_date}. Expected: \"YYYY-MM-DD HH:mm:SS.fff\" | \n error: {error}')
+    #   except Exception as error:
+    #       raise ValueError(f'Invalid format date of stop_date. Received: {destination_metadata_stop_date}. Expected: \"YYYY-MM-DD HH:mm:SS.fff\" | \n error: {error}')
 
-    #TO DO
+      #TO DO
     measurement_id = None
-    if len(execution.destination.destination_metadata) >= 9:
-      measurement_id = execution.destination.destination_metadata[8]
+    #if len(execution.destination.destination_metadata) >= 9:
+    #   measurement_id = execution.destination.destination_metadata[8]
     
     if not api_secret:
           raise ValueError(
@@ -149,7 +153,8 @@ class GoogleAnalytics4MeasurementProtocolUploaderDoFn(MegalistaUploader):
 
     
     for row in batch.elements:
-      timestamp_micros = row.get('timestamp_micros')
+      #timestamp_micros = row.get('timestamp_micros')
+
 
       # Petlove
       # logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').info(f'[PETLOVE] timestamp_micros: {timestamp_micros}, start_date: {start_date}, stop_date: {stop_date}')
@@ -164,6 +169,7 @@ class GoogleAnalytics4MeasurementProtocolUploaderDoFn(MegalistaUploader):
       app_instance_id = row.get('app_instance_id')
       client_id = row.get('client_id')
       user_id = row.get('user_id')
+      timestamp_micros = row.get('timestamp_micros')
         
 
       payload: Dict[str, Any] = {
@@ -176,6 +182,9 @@ class GoogleAnalytics4MeasurementProtocolUploaderDoFn(MegalistaUploader):
       
       if is_event:
         event_reserved_keys = self.reserved_keys + ['name']
+        #params = {k: v for k, v in row.items() if self._validate_param(k, v, event_reserved_keys)}
+        #payload['events'] = [{'name': row['name'], 'params': params}]
+        
         # params = {k: v for k, v in row.items() if self._validate_param(k, v, event_reserved_keys)}
         # payload['events'] = [{'name': row['name'], 'params': params}]
           
@@ -201,6 +210,8 @@ class GoogleAnalytics4MeasurementProtocolUploaderDoFn(MegalistaUploader):
       if is_user_property: 
         payload['userProperties'] = {k: {'value': v} for k, v in row.items() if self._validate_param(k, v, self.reserved_keys)}
         # Petlove
+        #payload['events'] = {'name': 'user_property_addition_event', 'params': {}}
+        
         payload['events'] = {'name': 'user_property_addition_event', 'params': {'currency': 'BRL'}}
 
       url_container = [f'{self.API_URL}?api_secret={api_secret}']
@@ -218,6 +229,7 @@ class GoogleAnalytics4MeasurementProtocolUploaderDoFn(MegalistaUploader):
             'GA4 MP needs an app_instance_id parameter when used for an App Stream.')
         payload['app_instance_id'] = app_instance_id
           
+      measurement_id = None
       if measurement_id:
         url_container.append(f'&measurement_id={measurement_id}')
         # Petlove
@@ -226,6 +238,8 @@ class GoogleAnalytics4MeasurementProtocolUploaderDoFn(MegalistaUploader):
           raise ValueError(
             'GA4 MP needs a client_id parameter when used for a Web Stream.')
         payload['client_id'] = client_id
+      else:
+        logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').info(f"[PETLOVE] measurement_id not in use")
 
       if user_id:
         payload['user_id'] = user_id
@@ -254,18 +268,19 @@ class GoogleAnalytics4MeasurementProtocolUploaderDoFn(MegalistaUploader):
         
       # logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').info(f"[PETLOVE] response.status_code: {response.status_code}")
         
-      if response.status_code != '204':
+      if response.status_code != 204:
         error_message = f'Error calling GA4 MP {response.status_code}: {str(response.content)}'
         logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').error(error_message)
         self._add_error(execution, error_message)
         
       else:
         logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').info(f"[PETLOVE] response.status_code: {response.status_code}")
-        logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').info(f"[PETLOVE] response.status_code TYPE: {type(response.status_code)}")
+        #logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').info(f"[PETLOVE] response.status_code TYPE: {type(response.status_code)}")
+
         
         logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').info(f"[PETLOVE] response.content: {response.content}")
-        logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').info(f"[PETLOVE] response.content TYPE: {type(response.content)}")
-        
+        #logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').info(f"[PETLOVE] response.content TYPE: {type(response.content)}")
+
           # Petlove
           # logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').info(f"[PETLOVE] status code: {response.status_code}")
           # logging.getLogger('megalista.GoogleAnalytics4MeasurementProtocolUploader').info(f"[PETLOVE] response: {response}")
