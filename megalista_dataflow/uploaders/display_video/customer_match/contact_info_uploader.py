004f92a19ec1f3ed75f6c6733c396ef2ddf34653
Pedro Lopes <6962758+Mr-Lopes@users.noreply.github.com>
2023-12-11 18:58:42 +0000

    Implements consent on DV

--- megalista_dataflow/uploaders/display_video/customer_match/contact_info_uploader.py
+++ megalista_dataflow/uploaders/display_video/customer_match/contact_info_uploader.py
@@ -25,6 +25,8 @@ from models.execution import DestinationType, AccountConfig
 class DisplayVideoCustomerMatchContactInfoUploaderDoFn(DisplayVideoCustomerMatchAbstractUploaderDoFn):
   def get_list_definition(self, account_config: AccountConfig, destination_metadata: List[str], list_to_add: List[Dict[str, Any]]) -> Dict[str, Any]:
     list_name = destination_metadata[1]
+    consent = self._get_consents(destination_metadata=destination_metadata)
+
     return {
       'displayName': list_name,
       'firstAndThirdPartyAudienceType': 'FIRST_AND_THIRD_PARTY_AUDIENCE_TYPE_FIRST_PARTY',
@@ -32,7 +34,8 @@ class DisplayVideoCustomerMatchContactInfoUploaderDoFn(DisplayVideoCustomerMatch
       'membershipDurationDays': 10000,
       'description': 'List created automatically by Megalista',
       'contactInfoList': {
-          'contactInfos': list_to_add
+          'contactInfos': list_to_add,
+          **(consent)
         }
     }
 
@@ -42,12 +45,31 @@ class DisplayVideoCustomerMatchContactInfoUploaderDoFn(DisplayVideoCustomerMatch
   def get_action_type(self) -> DestinationType:
     return DestinationType.DV_CUSTOMER_MATCH_CONTACT_INFO_UPLOAD
 
+  def _get_consents(self, destination_metadata: List[str]) -> Dict[str, str]:
+      if len(destination_metadata) >= 7:
+          if (
+              destination_metadata[5] is not None
+              and destination_metadata[6] is not None
+          ):
+              return {
+                  "consent": {
+                      "adUserData": destination_metadata[5],
+                      "adPersonalization": destination_metadata[6],
+                  },
+              }
+
+      return {}
+  
   def get_update_list_definition(self, account_config: AccountConfig, destination_metadata: List[str], list_to_add: List[Dict[str, Any]]) -> Dict[str, Any]:
     advertiser_id = destination_metadata[0]
+
+    consent = self._get_consents(destination_metadata=destination_metadata)
 
     return {
       'advertiserId': advertiser_id,
       'addedContactInfoList': {
-        'contactInfos': list_to_add
+        'contactInfos': list_to_add,
+        **(consent)
       }
     }
+  
