4af3c1a1776e08e87bc6434da79de6def8d2ccfe
Mr Lopes <6962758+Mr-Lopes@users.noreply.github.com>
2023-12-12 10:29:45 -0300

    Implements consent on DV (#156)

--- megalista_dataflow/uploaders/display_video/customer_match/contact_info_uploader_test.py
+++ megalista_dataflow/uploaders/display_video/customer_match/contact_info_uploader_test.py
@@ -175,3 +175,140 @@ def test_upload_update_users(mocker, uploader, error_notifier):
     )
 
     assert not error_notifier.were_errors_sent
+
+def test_upload_add_users_consent(mocker, uploader, error_notifier):
+
+    mocker.patch.object(uploader, '_get_dv_audience_service')
+    
+    audience = MagicMock() 
+    audience.firstAndThirdPartyAudienceId = 12345
+    audience.displayName = 'list_name'
+
+    uploader._get_dv_audience_service.return_value.list.return_value.execute.return_value = None
+    uploader._get_dv_audience_service.return_value.create.return_value.execute.return_value = audience
+    
+    destination = Destination(
+        'dest1',
+        DestinationType.DV_CUSTOMER_MATCH_CONTACT_INFO_UPLOAD,
+        ['advertiser_id', 'list_name', '', '', '', 'CONSENT_STATUS_GRANTED', 'CONSENT_STATUS_GRANTED']
+    )
+
+    source = Source('orig1', SourceType.BIG_QUERY, ['dt1', 'buyers'])
+    execution = Execution(_account_config, source, destination)
+
+    batch = Batch(execution, [{
+        'hashedFirstName': 'test-first-name',
+        'hashedLastName': 'test-last-name',
+        'countryCode': 'test-country-code',
+        'zipCodes': 'test-zipcode'
+    }])
+
+    uploader.process(batch)
+    uploader.finish_bundle()
+
+    uploader._get_dv_audience_service.return_value.list.assert_called_once_with(
+        advertiserId='advertiser_id',
+        pageSize=1,
+        filter='displayName : "list_name"'
+    )
+
+    test_create_resquest = {
+        'displayName': 'list_name',
+        'firstAndThirdPartyAudienceType': 'FIRST_AND_THIRD_PARTY_AUDIENCE_TYPE_FIRST_PARTY',
+        'audienceType': 'CUSTOMER_MATCH_CONTACT_INFO',
+        'membershipDurationDays': 10000,
+        'description': 'List created automatically by Megalista',
+        'contactInfoList': {
+            'contactInfos': [
+                {
+                    'hashedFirstName': 'test-first-name',
+                    'hashedLastName': 'test-last-name',
+                    'countryCode': 'test-country-code',
+                    'zipCodes': 'test-zipcode'
+                }
+            ],
+             'consent': {
+               'adUserData': 'CONSENT_STATUS_GRANTED',
+               'adPersonalization': 'CONSENT_STATUS_GRANTED'
+            }
+        }
+
+    }
+    
+    uploader._get_dv_audience_service.return_value.create.assert_called_once_with(
+        advertiserId='advertiser_id',
+        body=test_create_resquest
+    )
+
+    assert not error_notifier.were_errors_sent
+
+
+def test_upload_update_users_consent(mocker, uploader, error_notifier):
+
+    mocker.patch.object(uploader, '_get_dv_audience_service')
+    
+    audience = MagicMock() 
+    audience.firstAndThirdPartyAudienceId = 12345
+    audience.displayName = 'list_name'
+
+    audience_list = {
+        'firstAndThirdPartyAudiences': [
+            {
+                'firstAndThirdPartyAudienceId': 12345,
+                'displayName': 'list_name'
+            }
+        ]
+    }
+    
+    uploader._get_dv_audience_service.return_value.list.return_value.execute.return_value = audience_list
+    uploader._get_dv_audience_service.return_value.editCustomerMatchMembers.return_value = MagicMock()
+
+    destination = Destination(
+        'dest1',
+        DestinationType.DV_CUSTOMER_MATCH_CONTACT_INFO_UPLOAD,
+        ['advertiser_id', 'list_name', '', '', '', 'CONSENT_STATUS_GRANTED', 'CONSENT_STATUS_GRANTED']
+    )
+
+    source = Source('orig1', SourceType.BIG_QUERY, ['dt1', 'buyers'])
+    execution = Execution(_account_config, source, destination)
+
+    batch = Batch(execution, [{
+        'hashedFirstName': 'test-first-name',
+        'hashedLastName': 'test-last-name',
+        'countryCode': 'test-country-code',
+        'zipCodes': 'test-zipcode'
+    }])
+
+    uploader.process(batch)
+    uploader.finish_bundle()
+
+    uploader._get_dv_audience_service.return_value.list.assert_called_once_with(
+        advertiserId='advertiser_id',
+        pageSize=1,
+        filter='displayName : "list_name"'
+    )
+
+    test_update_resquest = {
+        'advertiserId': 'advertiser_id',
+        'addedContactInfoList': {
+            'contactInfos': [
+                {
+                    'hashedFirstName': 'test-first-name',
+                    'hashedLastName': 'test-last-name',
+                    'countryCode': 'test-country-code',
+                    'zipCodes': 'test-zipcode'
+                }
+            ],
+            'consent': {
+               'adUserData': 'CONSENT_STATUS_GRANTED',
+               'adPersonalization': 'CONSENT_STATUS_GRANTED'
+            }
+        }
+    }
+
+    uploader._get_dv_audience_service.return_value.editCustomerMatchMembers.assert_any_call(
+        firstAndThirdPartyAudienceId=audience_list['firstAndThirdPartyAudiences'][0]['firstAndThirdPartyAudienceId'],
+        body=test_update_resquest
+    )
+
+    assert not error_notifier.were_errors_sent
